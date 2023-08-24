ef45221b2a7b1cb30f218e88605bf58f05afdfb7
dayong <wangdayong228@163.com>
2023-08-24 11:41:02 +0800

    nil

---
+++ cmd/go.mod
@@ -0,0 +1,3 @@
+module github.com/wangdayong228/test-go-workspace/cmd
+
+go 1.20
---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package cmd
+
+import "github.com/wangdayong228/test-go-workspace/common"
+
+func main() {
+	common.Say("hello cmd")
+}
